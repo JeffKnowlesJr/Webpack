2cb68aabcc6e3063178a85a65b77c11b7f881dd5
JeffKnowlesJr <jeffknowlesjr@gmail.com>
2022-04-08 16:07:29 -0400

    config update

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 [Webpack Documentation](https://webpack.js.org/concepts/)
 
 - [Guides](https://webpack.js.org/guides/)
+
+- - [Getting Started](https://webpack.js.org/guides/getting-started/)
+
+- - [Using webpack-dev-server](https://webpack.js.org/guides/development/#using-webpack-dev-server)
--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,9 @@
-const path = require('path');
-
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-};
-const path = require('path');
-
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-};
-const path = require('path');
-
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-};
-const path = require('path');
-
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-};
-const path = require('path');
-
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-};
-const path = require('path');
-
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-};
-const path = require('path');
+const path = require('path')
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-};
+    path: path.resolve(__dirname, 'dist')
+  }
+}
